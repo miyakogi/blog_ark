d45bcdb6932e63947ea082a095574cdbe4d9645f
miyakogi <miyako.dev@gmail.com>
2016-02-15 12:33:07 +0900

    Update documentation

--- inc/menu.md
+++
@@ -1,7 +0,0 @@
----
-# This is a sample include file.
----
-
-* [Home](@root/)
-* [About](@root/about//)
-* [Posts](@root/posts/index//)
--- server.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from livereload import Server, shell
-
-server = Server()
-server.watch('./*.py', shell('ark build'))
-server.watch('./src/**/*.md', shell('ark build'))
-server.serve(port=8889, root='./out')
